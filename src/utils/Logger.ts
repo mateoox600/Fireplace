bd7d873be4f7727438667469dd5c39dcf0dbad07
mateoox600 <matboi862@gmail.com>
2022-01-19 10:25:24 +0100

    Added dynamic request method in logger

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -7,31 +7,27 @@ export default class Logger {
         Info: `${Colors.FgCyan}Info${Colors.Reset}`,
         Warning: `${Colors.FgYellow}Warning${Colors.Reset}`,
         Error: `${Colors.FgRed}Error${Colors.Reset}`,
-        Get: `${Colors.FgGreen}Get${Colors.Reset}`
+        Request: `${Colors.FgGreen}%r${Colors.Reset}`
     };
 
-    private print(str: string, prefix: keyof typeof this.Prefixs) {
-        process.stdout.write(`${this.Prefixs[prefix]} - ${str}\n`);
+    private print(str: string, prefix: keyof typeof this.Prefixs, f: (prefix: string) => string = (p) => p) {
+        process.stdout.write(`${f(this.Prefixs[prefix])} - ${str}\n`);
     }
 
     public log(str: string) {
         this.print(str, 'Info');
-        //console.log(`${Colors.FgCyan}Info${Colors.Reset} - ${str}`);
     }
 
     public warning(warn: string) {
         this.print(warn, 'Warning');
-        //console.log(`${Colors.FgYellow}Warning${Colors.Reset} - ${warn}`);
     }
 
     public error(err: string) {
         this.print(err, 'Error');
-        //console.log(`${Colors.FgRed}Error${Colors.Reset} - ${err}`);
     }
 
     public logCallback(req: Request, res: Response, next: NextFunction) {
-        this.print(req.originalUrl, 'Get');
-        //console.log(`${Colors.FgGreen}${req.method}${Colors.Reset} - ${req.originalUrl}`);
+        this.print(req.originalUrl, 'Request', (prefix) => prefix.replace('%r', req.method));
         next();
     }
 
